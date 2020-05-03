b1fa47f8a975a046c2a3542742e5c60bc0ee8a83
tonyb7 <bait@umich.edu>
2020-05-03 03:38:33 -0400

    more socket communication logic. register basic button callbacks

--- equations/views/networking.py
+++ equations/views/networking.py
@@ -2,14 +2,31 @@
 
 import flask
 import equations
+from flask_socketio import join_room, leave_room
 
 @equations.socketio.on('connect')
 def on_connect():
     """Handle request to connect to server."""
     print(f"Client {flask.request.sid} connected!")
 
+@equations.socketio.on('disconnect')
+def on_disconnect():
+    """Handle disconnect."""
+    print(f"Client {flask.request.sid} disconnected!")
+    # TODO leave_room()
+
 @equations.socketio.on('register_player')
 def register_player(json):
     """Register a player."""
-    print(f"Player {json['name']} wants to join room {json['room']}")
+    socketid = flask.request.sid
+    name = json['name']
+    room = json['room']
+
+    print(f"Client {socketid}: Player {name} wants to join room {room}")
+
+    # TODO: Error checking, save info in dict
+
+    join_room(room)
+
+
 
--- src/client/networking.js
+++ src/client/networking.js
@@ -13,9 +13,61 @@ const connectedPromise = new Promise(resolve => {
 
 export const connect = () => {
     connectedPromise.then(() => {
+        // Retrieve room and player info
         let room_nonce = document.getElementById("room-nonce").innerHTML;
         let name = document.getElementById("name").innerHTML;
-        console.log(`Connection was from ${name} in room ${room_nonce}`);
+
+        // Register callbacks
+        socket.on('disconnect', () => {
+            console.log(`${name} disconnected from room ${room_nonce}`);
+        });
+        registerButtonCallbacks();
+
+        // Tell server wanna join
+        console.log(`Connecting as ${name} in room ${room_nonce}`);
         socket.emit("register_player", {'room': room_nonce, 'name': name});
     });
+}
+
+function registerButtonCallbacks() {
+    var buttons = document.getElementById("buttons-div");
+    if (buttons.length === 0) {
+        console.log("Error finding buttons on page!?");
+        return;
+    }
+
+    let start_btn = buttons.querySelector("#start-game");
+    start_btn.onclick = () => {
+        console.log("Start game button clicked!");
+    };
+
+    let flip_timer_btn = buttons.querySelector('#flip-timer');
+    flip_timer_btn.onclick = () => {
+        console.log("Flip timer button clicked!");
+    };
+
+    let claim_warning_btn = buttons.querySelector("#claim-warning");
+    claim_warning_btn.onclick = () => {
+        console.log("Claim 10 second warning button clicked!");
+    };
+
+    let claim_minus_one_btn = buttons.querySelector('#claim-minus-one');
+    claim_minus_one_btn.onclick = () => {
+        console.log("Claim minus one button clicked!");
+    };
+
+    let a_flub_btn = buttons.querySelector('#a-flub');
+    a_flub_btn.onclick = () => {
+        console.log("Challenge now button clicked!");
+    }; 
+
+    let p_flub_btn = buttons.querySelector('#p-flub');
+    p_flub_btn.onclick = () => {
+        console.log("Challenge impossible button clicked!");
+    }; 
+
+    let force_out_btn = buttons.querySelector('#force-out');
+    force_out_btn.onclick = () => {
+        console.log("Force out button clicked!");
+    };
 }
