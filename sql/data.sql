4832422a8356b73bb838db675b4d6ead95ca670b
tonyb7 <bait@umich.edu>
2020-05-04 21:03:25 -0400

    add an account to data.sql for testing. a longer name.

--- sql/data.sql
+++ sql/data.sql
@@ -2,7 +2,8 @@ PRAGMA foreign_keys = ON;
 
 INSERT INTO users(username, password)
 VALUES ('Server', 'sha512$bc01cc83f48a4f9692d79b7a7a2e3eae$a2cadd82b348fc1516da2832072bec487cf7deed04dce75d4bea014e5a73d6ab1ceba676030fdf9177098f332997c70f4d04aa2eaa35a3f638a8c9a6f08eb5c6'),
-	('Tony', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8');
+	('Tony', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8'),
+	('FreezingTiger', 'sha512$a45ffdcc71884853a2cba9e6bc55e812$c739cef1aec45c6e345c8463136dc1ae2fe19963106cf748baf87c7102937aa96928aa1db7fe1d8da6bd343428ff3167f4500c8a61095fb771957b4367868fb8');
 	
 INSERT INTO games(nonce, started, players, spectators)
 VALUES('example', 1, "Tony,James,Ethan", "Kern,Danny,Brian");
