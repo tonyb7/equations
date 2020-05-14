7310d041416c7a63e7ab5e4c96c9fb2c92cc4ac2
tonyb7 <bait@umich.edu>
2020-05-14 00:17:11 -0400

    create db for now

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Launch script for Heroku
 
-# Create db only first time
-# bin/equationsdb create
+# Create db
+bin/equationsdb create
 
 # Launch server
 gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 equations:app
